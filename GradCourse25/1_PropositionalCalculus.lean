b678a041bc93e3018bc27b279ce1dc356119449a
Emily <emily.beatty@ens-lyon.fr>
2025-02-19 15:47:28 +0100

    mid first class commit

--- GradCourse25/1_PropositionalCalculus.lean
+++ GradCourse25/1_PropositionalCalculus.lean
@@ -5,7 +5,8 @@ import Mathlib.Tactic
 variable (P Q R S : Prop)
 
 -- **ToDo**
-example (n : ℕ) (hn : n ≤ 3) : n ≤ 5 := by sorry
+example (n : ℕ) (hn : n ≤ 3) : n ≤ 5 := by
+    linarith
 
 
 -- `⌘`
@@ -30,38 +31,60 @@ example (n : ℕ) (hn : n ≤ 3) : n ≤ 5 := by sorry
 
 -- Use of the `exact` tactic
 -- **ToDo**
-example (hP : P) : P := by sorry
+example (hP : P) : P := by
+    exact hP
 
 -- Use of the `intro` tactic
 -- **ToDo**
-example : P → P := by sorry
+example : P → P := by
+    intro hP
+    exact hP
 
 -- Use of the `apply` tactic
 -- **ToDo**
-example (h : P → Q) (hP : P) : Q := by sorry
+example (h : P → Q) (hP : P) : Q := by
+    apply h
+    exact hP
 
 -- Use `\.` to write `·`
 -- **ToDo**
-example : (P → Q → R) → ((P → Q) → (P → R)) := by sorry
+example : (P → Q → R) → ((P → Q) → (P → R)) := by
+  intro PimpQimpR PimpQ hP
+  apply PimpQimpR
+  · exact hP
+  · apply PimpQ
+    exact hP
 
 
 -- Modus Ponens: if `P → Q` then `Q` can be deduced from `P`
 -- **Exercise**
-example : P → (P → Q) → Q := by sorry
+example : P → (P → Q) → Q := by
+  intro hP PimpQ
+  apply PimpQ
+  exact hP
 
 -- Transitivity of `→`
 -- **Exercise**
-example : (P → Q) → (Q → R) → P → R := by sorry
+example : (P → Q) → (Q → R) → P → R := by
+  intro PimpQ QimpR hP
+  apply QimpR
+  apply PimpQ
+  exact hP
 
 -- Use of the `rfl` tactic
 -- **ToDo**
-example : P = P := by sorry
+example : P = P := by
+  rfl
 
 -- **Exercise**
-example (hP : P) : Q → (hP = hP) := by sorry
+example (hP : P) : Q → (hP = hP) := by
+  intro
+  rfl
 
 -- **Exercise**
-example (hP : P) : R → P → Q → (hP = hP) := by sorry
+example (hP : P) : R → P → Q → (hP = hP) := by
+  intros
+  rfl
 
 
 -- `⌘`
@@ -71,21 +94,30 @@ example (hP : P) : R → P → Q → (hP = hP) := by sorry
 
 -- `P` is not a proposition: it is a True/False statement for terms in `α`.
 -- **ToDo**
-example (α : Type) (P : α → Prop) (x y : α) (hx : P x) (h : y = x) : P y := by sorry
+example (α : Type) (P : α → Prop) (x y : α) (hx : P x) (h : y = x) : P y := by
+  rw [h]
+  exact hx
 
 
 -- **ToDo**
-example (α : Type) (P Q : α → Prop) (x : α) (hP : P x) (h : P = Q) : Q x := by sorry
+example (α : Type) (P Q : α → Prop) (x : α) (hP : P x) (h : P = Q) : Q x := by
+  rw [← h]
+  exact hP
 
 -- **Exercise**
-example (n : ℕ) (h : n = 5) : n = 2 + 3 := by sorry
+example (n : ℕ) (h : n = 5) : n = 2 + 3 := by
+  rw [h]
 
 -- **Exercise**
-example (n m : ℕ) (hn : n = 5) (hm : 11 = m) : m = n + 6 := by sorry
+example (n m : ℕ) (hn : n = 5) (hm : 11 = m) : m = n + 6 := by
+  rw [hn]
+  rw [← hm]
 
 -- **Exercise**
-example (α : Type) (a b c : α) (H : (a = b) → P ) (h1 : c = a) (h2 : b = c) : P := by sorry
-
+example (α : Type) (a b c : α) (H : (a = b) → P ) (h1 : c = a) (h2 : b = c) : P := by
+  apply H
+  rw [← h1]
+  rw [h2]
 
 -- `⌘`
 
@@ -93,33 +125,64 @@ example (α : Type) (a b c : α) (H : (a = b) → P ) (h1 : c = a) (h2 : b = c) 
 /- # `True`, `False`, negation, contradiction -/
 
 -- **ToDo**
-example : True := by sorry
-
+example : True := by
+  trivial
 -- **Exercise**
-example : True → True := by sorry
+example : True → True := by
+  intro
+  trivial
 
 -- Use of the `exfalso` tactic
 -- **ToDo**
-example : False → P := by sorry
+example : False → P := by
+  intro h
+  exfalso
+  exact h
 
 -- **Exercise**
-example : (P → False) → P → Q := by sorry
+example : (P → False) → P → Q := by
+  intro PimpF hP
+  exfalso
+  apply PimpF
+  exact hP
 
 -- type `¬` by typing `\not`.
 -- **ToDo**
-example : P → Q → P → ¬ Q → ¬ P := by sorry
+example : P → Q → P → ¬ Q → ¬ P := by
+  intro hP hQ hP' hnQ habs
+  apply hnQ
+  exact hQ
+
 -- **Exercise**
-example : P → ¬ P → False := by sorry
+example : P → ¬ P → False := by
+  intro hP hnP
+  apply hnP
+  exact hP
 
 -- Use of the `by_contra` tactic
 -- **ToDo**
-example : (¬Q → ¬P) → P → Q := by sorry
+example : (¬Q → ¬P) → P → Q := by
+  intro h hP
+  by_contra h_abs
+  apply h
+  · exact h_abs
+  · exact hP
 
 -- **Exercise**
-example : (P → ¬ Q) → (Q → ¬ P) := by sorry
+example : (P → ¬ Q) → (Q → ¬ P) := by
+  intro PimpnQ hQ
+  by_contra h_abs
+  apply PimpnQ
+  · exact h_abs
+  · exact hQ
+
 
 -- **Exercise**
-example (h : ¬ (2 = 2)) : P → Q := by sorry
+example (h : ¬ (2 = 2)) : P → Q := by
+  intro hP
+  by_contra h_abs
+  apply h
+  rfl
 
 
 -- `⌘`
@@ -129,16 +192,26 @@ example (h : ¬ (2 = 2)) : P → Q := by sorry
   Use `\and` to write `∧` -/
 
 -- **ToDo**
-example : P → Q → P ∧ Q := by sorry
+example : P → Q → P ∧ Q := by
+  intro hP hQ
+  constructor
+  · exact hP
+  · exact hQ
 
 -- **ToDo**
-example : P ∧ Q → P := by sorry
+example : P ∧ Q → P := by
+  intro hPaQ
+  exact hPaQ.left
 
 -- **Exercise**
-example : P ∧ Q → Q := by sorry
+example : P ∧ Q → Q := by
+  intro hPaQ
+  exact hPaQ.right
 
 -- **Exercise**
-example : (P → Q → R) → P ∧ Q → R := by sorry
+example : (P → Q → R) → P ∧ Q → R := by
+  intro PimpQimpR hPaQ
+  sorry
 
 -- `∧` is symmetric
 -- **Exercise**
